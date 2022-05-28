b89e9a8d9568ef3a3a13589d3350fd220009beea
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-05-28 14:34:39 +0900

    Separated JS files

---
+++ main.js
@@ -0,0 +1,95 @@
+const todos = [
+  {
+    id: '',
+    done: false,
+    title: '',
+    content: ''
+  }
+]
+
+const myData = {
+  appName: 'TODOアプリ',
+  todos,
+  done: false,
+  edit: 0,
+  title: '',
+  content: ''
+}
+
+const app = Vue.createApp({
+  data () {
+    return myData
+  },
+  created () {
+    console.log('created!!')
+    this.loadTodos()
+  },
+
+  methods: {
+    switchTodo: function () {
+      this.done = !this.done
+      this.saveTodos()
+    },
+
+    pushTodo: function () {
+      this.done = false
+      const today = Date.now()
+      const todo = {
+        id: today,
+        done: false,
+        title: this.title = [] ? '(未入力)' : this.title,
+        content: this.content
+      }
+      this.todos.push(todo)
+      this.saveTodos()
+    },
+
+    loadTodos: function () {
+      const json = localStorage.getItem('todo')
+      if (json != null) this.todos = JSON.parse(json)
+    },
+
+    saveTodos: function () {
+      const json = JSON.stringify(this.todos)
+      localStorage.setItem('todo', json)
+      this.dataReset()
+    },
+
+    changeTodo: function (id) {
+      console.log('changeTodo:' + id)
+      this.todos.forEach(todo => {
+        if (todo.id === id) todo.done = !todo.done
+        this.saveTodos()
+      })
+    },
+    editTodo: function (id) {
+      console.log('editTodo:' + id)
+      this.edit = id
+    },
+
+    deleteTodo: function (id) {
+      this.todos = this.todos.filter(todo => {
+        return todo.id !== id
+      })
+      this.saveTodos()
+    },
+
+    dataReset: function () {
+      this.title = null
+      this.content = null
+    }
+
+  },
+  computed: {
+    getTodos () {
+      const extractTodos = this.todos.filter(todo => {
+        return this.done === todo.done
+      })
+      extractTodos.sort(function (a, b) {
+        return b.id - a.id
+      })
+      return extractTodos
+    }
+  }
+})
+app.mount('#app')
